f643d9dfb84d078c766a2b0a458e02834276ee74
alfonso altamirano <alfonso_lolxd@outlook.com>
2019-09-05 16:11:14 -0500

    agrege P.2

--- catkin_ws/src/students/altamirano_leal/scripts/practice00.py
+++ catkin_ws/src/students/altamirano_leal/scripts/practice00.py
@@ -16,20 +16,41 @@ from geometry_msgs.msg import Twist
 NAME = "Alfonso Altamirano"
 
 def callback_laser_scan(msg):
+
+	global objeto_detectado #variable global 
+	
+	if msg.ranges[len(msg.ranges)/2]<1.0: # ranges da la longuitud de un areglo en este el areglo de los angulos
+	
+		print "Obstaculo detectado"
+		objeto_detectado=True
+	else:
+		objeto_detectado=False
     #
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     #
-    return
 
 def main():
     print "PRACTICE 00 - " + NAME
     rospy.init_node("practice00")
+    #subscriptor p
     rospy.Subscriber("/hardware/scan", LaserScan, callback_laser_scan)
+    #publicador publica una x y z 
     pub_cmd_vel = rospy.Publisher("/hardware/mobile_base/cmd_vel", Twist, queue_size=10)
     loop = rospy.Rate(10)
     
+    cmd_vel=Twist()
+    global objeto_detectado 
+    objeto_detectado=False
+
     while not rospy.is_shutdown():
+
+    	if objeto_detectado:
+    		cmd_vel.linear.x=0
+
+    	else:
+    		cmd_vel.linear.x=0.5 #velocidad lineal = .5
+    	pub_cmd_vel.publish(cmd_vel) #publicar los valores de la velocidad para la coneccion a la ros
         #
         # TODO:
         # Declare a Twist message and assign the appropiate speeds:
