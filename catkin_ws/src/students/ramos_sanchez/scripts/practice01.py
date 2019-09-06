3eade9494b9ebdf2ac4fc301c8f585a82e9a533f
samrs <samuel.rkz12@gmail.com>
2019-09-05 21:41:01 -0500

    DEPTH FIRST SEARCH

--- catkin_ws/src/students/ramos_sanchez/scripts/practice01.py
+++ catkin_ws/src/students/ramos_sanchez/scripts/practice01.py
@@ -16,85 +16,140 @@ from navig_msgs.srv import CalculatePath
 from navig_msgs.srv import CalculatePathResponse
 from collections import deque
 
-NAME = "RAMOS_SANCHEZ_SAMUEL"
+NAME = "RAMOS_SANCHEZ"
 
+#FIFO
 def callback_bfs(req):
-    #este request esta relacionado a CalculatePath, esta ubicada la biblioteca en
-    #src/navigation
     print "Calculating path by breadth first search"
     steps = 0
-    n = -1
-    msg_path = Path()
-    open_list = deque()
-    map_width = req.map.info.width
-
-    is_in_open_list = [False]*(map_width*req.map.info.height)
-    is_in_close_list = [False]*(map_width*req.map.info.height)
-    n_values = [sys.maxint]*(map_width*req.map.info.height)
-    previous = [-1]*(map_width*req.map.info.height)
-    start_x = req.start.pose.position.x
-    start_y = req.start.pose.position.y
-    goal_x = req.goal.pose.position.x
-    goal_y = req.goal.pose.position.y
-    start_idx = int((start_y - req.map.info.origin.position.y)/req.map.info.resolution)*map_width
-    start_idx += int((start_x - req.map.info.origin.position.x)/req.map.info.resolution)
-    goal_idx = int((goal_y - req.map.info.origin.position.y)/req.map.info.resolution)*map_width
-    goal_idx += int((goal_x - req.map.info.origin.position.x)/req.map.info.resolution)
-
-    open_list.append(start_idx)
-    while(len(open_list) != 0 and n != goal_idx):
-        n = open_list.popleft()
-        neighbors = [n + map_width, n + 1, n - map_width, n - 1]
-        for x in neighbors:
-            if not is_in_open_list[x] and not is_in_closed_list[x]:
-                open_list.append(x)
-                is_in_open_list[x] = True
-            if n_values[x] > n_values[n] + 1:
-                n_values[x] = n_values[n] + 1
-                previous[x] = n
-                  
-        is_in_closed_list[n] = True
-
-    if n == goal_idx:
-        print 'Path found'
-        while previous[n] != -1:
-            print n
-            n = previous[n]
-    else: 
-        print 'Path not found'
-    #Traducir las celdas a valores X,Y     
+    #
     # TODO:
-    # Write a breadth first search algorithm to find a path between the start position given by
+    # Write a breadth first search algorithm to find a path between the start position
     # [req.start.pose.position.x, req.start.pose.position.y]
-    # and the goal position, given by
+    # and the goal position
     # [req.goal.pose.position.x, req.goal.pose.position.y]
     # Use the 'steps' variable to store the total steps needed for calculations
+    #
+    start_idx  = int((req.start.pose.position.x - req.map.info.origin.position.x)/req.map.info.resolution)
+    start_idx += int((req.start.pose.position.y - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
+    goal_idx   = int((req.goal.pose.position.x  - req.map.info.origin.position.x)/req.map.info.resolution)
+    goal_idx  += int((req.goal.pose.position.y  - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
     
+    open_list      = deque()
+    in_open_list   = [False]*len(req.map.data)
+    in_closed_list = [False]*len(req.map.data)
+    distances      = [sys.maxint]*len(req.map.data)
+    parent_nodes   = [-1]*len(req.map.data)
+    
+    current = start_idx
+    open_list.append(start_idx)
+    in_open_list[start_idx] = True
+    distances[start_idx]    = 0
+
+    while len(open_list) > 0 and current != goal_idx:
+        current = open_list.popleft()
+        in_closed_list[current] = True
+        neighbors = [current + req.map.info.width, current - req.map.info.width, current + 1, current - 1]
+        dist = distances[current] + 1
+        for n in neighbors:
+            if req.map.data[n] > 40 or req.map.data[n] < 0 or in_closed_list[n]:
+                continue
+            if dist < distances[n]:
+                distances[n]    = dist
+                parent_nodes[n] = current
+            if not in_open_list[n]:
+                in_open_list[n] = True
+                open_list.append(n)
+            steps += 1
+
+    if current != goal_idx:
+        print "Cannot calculate path :'("
+        return None
+    ####
     print "Path calculated after " + str(steps) + " steps."
+    msg_path = Path()
     msg_path.header.frame_id = "map"
+    #
     # TODO:
-    # Store the resulting path in the 'msg_path' variable (see online documentation for nav_msgs/Path)
+    # Store the resulting path in the 'msg_path' variable
     # Return the appropiate response.
+    #
+    while parent_nodes[current] != -1:
+        p = PoseStamped()
+        p.pose.position.x = (current%req.map.info.width)*req.map.info.resolution + req.map.info.origin.position.x
+        p.pose.position.y = (current/req.map.info.width)*req.map.info.resolution + req.map.info.origin.position.y
+        msg_path.poses.insert(0,p)
+        current = parent_nodes[current]
+    ####
     pub_path = rospy.Publisher('/navigation/path_planning/calculated_path', Path, queue_size=10)
     pub_path.publish(msg_path)
     return CalculatePathResponse(msg_path)
 
+#LIFO
 def callback_dfs(req):
     print "Calculating path by depth first search" ####
     steps = 0
-    msg_path = Path()
+    #
     # TODO:
     # Write a depth first search algorithm to find a path between the start position
     # [req.start.pose.position.x, req.start.pose.position.y]
     # and the goal position
     # [req.goal.pose.position.x, req.goal.pose.position.y]
     # Use the 'steps' variable to store the total steps needed for calculations
+    #
     
+    start_idx  = int((req.start.pose.position.x - req.map.info.origin.position.x)/req.map.info.resolution)
+    start_idx += int((req.start.pose.position.y - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
+    goal_idx   = int((req.goal.pose.position.x  - req.map.info.origin.position.x)/req.map.info.resolution)
+    goal_idx  += int((req.goal.pose.position.y  - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
+    
+    open_list      = deque()
+    in_open_list   = [False]*len(req.map.data)
+    in_closed_list = [False]*len(req.map.data)
+    distances      = [sys.maxint]*len(req.map.data)
+    parent_nodes   = [-1]*len(req.map.data)
+    
+    current = start_idx
+    open_list.append(start_idx)
+    in_open_list[start_idx] = True
+    distances[start_idx]    = 0
+
+    while len(open_list) > 0 and current != goal_idx:
+        current = open_list.pop() #decouple the last value that entered in the list
+        in_closed_list[current] = True
+        neighbors = [current + req.map.info.width, current - req.map.info.width, current + 1, current - 1]
+        dist = distances[current] + 1
+        for n in neighbors:
+            if req.map.data[n] > 40 or req.map.data[n] < 0 or in_closed_list[n]:
+                continue
+            if dist < distances[n]:
+                distances[n]    = dist
+                parent_nodes[n] = current
+            if not in_open_list[n]:
+                in_open_list[n] = True
+                open_list.append(n)
+            steps += 1
+
+    if current != goal_idx:
+        print "Cannot calculate path :'("
+        return None
+
+    ####
     print "Path calculated after " + str(steps) + " steps."
+    msg_path = Path()
     msg_path.header.frame_id = "map"
+    #
     # TODO:
     # Store the resulting path in the 'msg_path' variable
     # Return the appropiate response.
+    #
+    while parent_nodes[current] != -1:
+        p = PoseStamped()
+        p.pose.position.x = (current%req.map.info.width)*req.map.info.resolution + req.map.info.origin.position.x
+        p.pose.position.y = (current/req.map.info.width)*req.map.info.resolution + req.map.info.origin.position.y
+        msg_path.poses.insert(0,p)
+        current = parent_nodes[current]
+    ####
     pub_path = rospy.Publisher('/navigation/path_planning/calculated_path', Path, queue_size=10)
     pub_path.publish(msg_path)
     return CalculatePathResponse(msg_path)
