3f409a6878d7816b09330f21a6c7ca51688a8297
HillaryGil97 <54074720+HillaryGil97@users.noreply.github.com>
2019-09-05 21:22:36 -0500

    practica 00 terminada

--- catkin_ws/src/students/gil_gonzalez/scripts/practice00.py
+++ catkin_ws/src/students/gil_gonzalez/scripts/practice00.py
@@ -9,26 +9,35 @@
 # Required publishers and subscribers are already declared and initialized.
 #
 
-import rospy
+import rospy #libreria para poder utilizar las funciones de ros 
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
 NAME = "Gil_Gonzalez"
 
-def callback_laser_scan(msg):
+def callback_laser_scan(msg):#msg es una variable de tipo laserScan
     #
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     #
+    global obstacle_detected #variable global para saber si se detuvo el obstaculo
+    if (msg.ranges[len(msg.ranges)/2]<1.0):
+        print "Obstaculo detectado"
+        obstacle_detected=True
+    else:
+        obstacle_detected=False
     return
 
 def main():
     print "PRACTICE 00 - " + NAME
     rospy.init_node("practice00")
-    rospy.Subscriber("/hardware/scan", LaserScan, callback_laser_scan)
-    pub_cmd_vel = rospy.Publisher("/hardware/mobile_base/cmd_vel", Twist, queue_size=10)
+    rospy.Subscriber("/hardware/scan", LaserScan, callback_laser_scan)#crea un subscriptor
+    pub_cmd_vel = rospy.Publisher("/hardware/mobile_base/cmd_vel", Twist, queue_size=10)#este es el publicador
     loop = rospy.Rate(10)
     
+    cmd_vel=Twist()
+    global obstacle_detected
+    obstacle_detected=False
     while not rospy.is_shutdown():
         #
         # TODO:
@@ -36,6 +45,12 @@ def main():
         # Move forward if there is no obstacle in front and stop otherwise.
         # Publish the message.
         #
+        
+        if obstacle_detected:
+            cmd_vel.linear.x=0
+        else:
+            cmd_vel.linear.x=0.5
+        pub_cmd_vel.publish(cmd_vel)
         loop.sleep()
 
 
