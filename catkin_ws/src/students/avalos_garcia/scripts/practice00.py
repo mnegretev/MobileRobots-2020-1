bf88ff1d50ecfbb9de3f95f7a4515f509279eef1
savag <savag@outlook.com>
2019-09-09 00:03:15 -0500

    Práctica00

--- catkin_ws/src/students/avalos_garcia/scripts/practice00.py
+++ catkin_ws/src/students/avalos_garcia/scripts/practice00.py
@@ -13,14 +13,21 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "AVALOS_GARCIA"
 
-def callback_laser_scan(msg):
+def callback_laser_scan(msg): #It is a parallel event which determines if there is an obstacle, at each laser measure.
     #
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     #
+    global obstacle_detected #Boolean variable to determine if obstacle is being detected
+    if msg.ranges[len(msg.ranges)/2]<1.0: #It retrieves the data from the laser, since it is the trayectory from the robot to the wall and the return from the wall to the robot, it must be divided by two. 1.0 represents a distance in meters.
+	print "Obstacle detected"
+	obstacle_detected=True
+    else:
+	obstacle_detected=False
     return
+    #     
 
 def main():
     print "PRACTICE 00 - " + NAME
@@ -28,7 +35,11 @@ def main():
     rospy.Subscriber("/hardware/scan", LaserScan, callback_laser_scan)
     pub_cmd_vel = rospy.Publisher("/hardware/mobile_base/cmd_vel", Twist, queue_size=10)
     loop = rospy.Rate(10)
-    
+    #
+    cmd_vel=Twist() #cmd_vel is subscribed to Twist that expresses velocity in free space broken into its linear and angular parts. 
+    global obstacle_detected #Declaration of variable in main.
+    obstacle_detected=False #As an initial condition, it is going to be uploaded with the sensor information.
+    #
     while not rospy.is_shutdown():
         #
         # TODO:
@@ -36,8 +47,13 @@ def main():
         # Move forward if there is no obstacle in front and stop otherwise.
         # Publish the message.
         #
-        loop.sleep()
-
+	if obstacle_detected:
+		cmd_vel.linear.x=0.0 #If the laser detects an obstacle stops.
+	else:
+		cmd_vel.linear.x=0.5 #If the laser does not detect an obstacle keeps moving.
+	pub_cmd_vel.publish(cmd_vel) #pub_cmd_vel publishes cmd_vel value.
+        loop.sleep() #It is used to keep rate during loop.  
+        #
 
 if __name__ == '__main__':
     try:
---
+++ catkin_ws/src/students/avalos_garcia/scripts/practice02.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+#
+# AUTONOMOUS MOBILE ROBOTS - UNAM, FI, 2020-1
+# PRACTICE 2 - PATH PLANNING BY DIJKSTRA AND A-STAR WITH MAP INFLATION AND NEARNESS
+#
+# Instructions:
+# Write the code necessary to plan a path using 
+# Dijkstra and A star.
+# Before planning, map obstacles must be inflated and nearness should be taken into account
+# as part of the cost function.
+#
+
+import sys
+import copy
+import rospy
+import heapq
+from collections import deque
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import Path
+from navig_msgs.srv import CalculatePath
+from navig_msgs.srv import CalculatePathResponse
+
+NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+
+def inflate_map(map):
+    if rospy.has_param("/navigation/path_planning/inflation_radius"):
+        radius = float(rospy.get_param("/navigation/path_planning/inflation_radius"))
+    else:
+        radius = 1.0        
+    print "Inflating map by " + str(radius) + " meters"
+    inflated = OccupancyGrid()
+    #
+    # TODO:
+    # Inflate all obstacles in 'map' by 'radius'
+    # Store the resulting inflated map in 'inflated'
+    #
+    
+    ####
+    return inflated
+
+def get_nearness(map):
+    if rospy.has_param("/navigation/path_planning/nearness_radius"):
+        radius = float(rospy.get_param("/navigation/path_planning/nearness_radius"))
+    else:
+        radius = 1.0
+    print "Calculating nearness for a radius of " + str(radius) + " m"
+    nearness = OccupancyGrid()
+    #
+    # TODO:
+    # Get a map where all free cells within a 'radius' distance from an obstacle
+    # contain a number indicating the distance to such obstacles
+    # Store the resulting map in 'nearness'
+    #
+    return nearness
+
+def callback_dijkstra(req):
+    print "Calculating path by Dijkstra search"###
+    map = inflate_map(req.map)
+    map = get_nearness(map)
+    steps = 0
+    #
+    # TODO:
+    # Write a Dijkstra algorithm to find a path between the start position
+    # [req.start.pose.position.x, req.start.pose.position.y]
+    # and the goal position
+    # [req.goal.pose.position.x, req.goal.pose.position.y]
+    # Consider nearness and distance traveled as cost function.
+    # Use the 'steps' variable to store the total steps needed for calculations
+    # HINT: Use a heap structure to keep track of the node with the smallest cost function
+    #
+    
+    ####
+    print "Path calculated after " + str(steps) + " steps."
+    msg_path = Path()
+    msg_path.header.frame_id = "map"
+    #
+    # TODO:
+    # Store the resulting path in the 'msg_path' variable
+    # Return the appropiate response
+    # 
+    
+    ####
+    pub_path = rospy.Publisher('/navigation/path_planning/calculated_path', Path, queue_size=10)
+    pub_path.publish(msg_path)
+    return CalculatePathResponse(msg_path)
+
+def callback_a_star(req):
+    print "Calculating path A-Star" ####
+    map = inflate_map(req.map)
+    map = get_nearness(map)
+    steps = 0
+    #
+    # TODO:
+    # Write a A-star algorithm to find a path between the start position
+    # [req.start.pose.position.x, req.start.pose.position.y]
+    # and the goal position
+    # [req.goal.pose.position.x, req.goal.pose.position.y]
+    # Consider nearness and distance traveled as cost function and Manhattan distance to goal as h-value
+    # Use the 'steps' variable to store the total steps needed for calculations
+    # HINT: Use a heap structure to keep track of the node with the smallest f-value
+    #
+    
+    ####
+    print "Path calculated after " + str(steps) + " steps."
+    msg_path = Path()
+    msg_path.header.frame_id = "map"
+    #
+    # TODO:
+    # Store the resulting path in the 'msg_path' variable
+    # Return the appropiate response
+    #
+    
+    ####
+    pub_path = rospy.Publisher('/navigation/path_planning/calculated_path', Path, queue_size=10)
+    pub_path.publish(msg_path)
+    return CalculatePathResponse(msg_path)
+
+def main():
+    print "PRACTICE 02 - " + NAME
+    rospy.init_node("practice02")
+    rospy.Service('/navigation/path_planning/dijkstra_search', CalculatePath, callback_dijkstra)
+    rospy.Service('/navigation/path_planning/a_star_search'  , CalculatePath, callback_a_star)
+    rospy.wait_for_service('/navigation/localization/static_map')
+    loop = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        loop.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+    
