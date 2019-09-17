81d454a3b95d9f9fa4983ccd82beaa88baff5a5e
Victor Jesus <cnvjgbr_17@hotmail.com>
2019-09-16 23:08:48 -0500

    Práctica 2 corregida, método dijkstra y A*

--- catkin_ws/src/students/cruz_navarrete/scripts/practice02.py
+++ catkin_ws/src/students/cruz_navarrete/scripts/practice02.py
@@ -7,7 +7,8 @@
 # Write the code necessary to plan a path using 
 # Dijkstra and A star.
 # Before planning, map obstacles must be inflated and nearness should be taken into account
-# as part of the cost function. 
+# as part of the cost function.
+#
 
 import sys
 import copy
@@ -19,15 +20,14 @@ from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import Path
 from navig_msgs.srv import CalculatePath
 from navig_msgs.srv import CalculatePathResponse
-import math
 
 NAME = "CRUZ_NAVARRETE"
 
 def inflate_map(map):
     if rospy.has_param("/navigation/path_planning/inflation_radius"):
-        radius = float(rospy.get_param("/navigation/path_planning/inflation_radius"))
+        radius = float(rospy.get_param("/navigation/path_planning/inflation_radius")) #con esto podemos cambiar el valor de la inflacio desde la interfaz grafica
     else:
-        radius = 1.0        
+        radius = 1.0 #si no cambiamos el valor de la inflacion desde la interfaz grafica, queremos que este sea del radio del robot, es decir 1 metro.
     print "Inflating map by " + str(radius) + " meters"
     inflated = OccupancyGrid()
     #
@@ -36,7 +36,7 @@ def inflate_map(map):
     # Store the resulting inflated map in 'inflated'
     #
     
-    ##
+    ####
     return inflated
 
 def get_nearness(map):
@@ -44,7 +44,7 @@ def get_nearness(map):
         radius = float(rospy.get_param("/navigation/path_planning/nearness_radius"))
     else:
         radius = 1.0
-    print "Calculating nearness for a radius of " + str(radius) + " m"
+    print "Calculating nearness for a radius of " + str(radius) + " meters"
     nearness = OccupancyGrid()
     #
     # TODO:
@@ -68,50 +68,42 @@ def callback_dijkstra(req):
     # Consider nearness and distance traveled as cost function.
     # Use the 'steps' variable to store the total steps needed for calculations
     # HINT: Use a heap structure to keep track of the node with the smallest cost function
+    #
+    start_idx  = int((req.start.pose.position.x - map.info.origin.position.x)/map.info.resolution)
+    start_idx += int((req.start.pose.position.y - map.info.origin.position.y)/map.info.resolution)*map.info.width
+    goal_idx   = int((req.goal.pose.position.x  - map.info.origin.position.x)/map.info.resolution)
+    goal_idx  += int((req.goal.pose.position.y  - map.info.origin.position.y)/map.info.resolution)*map.info.width
     
-    start_idx  = int((req.start.pose.position.x - req.map.info.origin.position.x)/req.map.info.resolution)
-    start_idx += int((req.start.pose.position.y - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
-    goal_idx   = int((req.goal.pose.position.x  - req.map.info.origin.position.x)/req.map.info.resolution)
-    goal_idx  += int((req.goal.pose.position.y  - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
-
-    open_list      = []
-    #open_list = deque()
-    in_open_list   = [False]*len(req.map.data)
-    in_closed_list = [False]*len(req.map.data)
-    distances      = [sys.maxint]*len(req.map.data)
-    parent_nodes   = [-1]*len(req.map.data)
+    open_list      = []###
+    in_open_list   = [False]*len(map.data)
+    in_closed_list = [False]*len(map.data)
+    distances      = [sys.maxint]*len(map.data)
+    parent_nodes   = [-1]*len(map.data)
     
     current = start_idx
-    heapq.heappush(open_list,start_idx) #aqui agregamos el nodo inicial a la lista abierta, se ordema de modo que le menor número queda la inicio de la cola
-    #open_list.append(start_idx)
+    heapq.heappush(open_list, (0, start_idx))##
     in_open_list[start_idx] = True
     distances[start_idx]    = 0
-    
+
     while len(open_list) > 0 and current != goal_idx:
-        current = heapq.heappop(open_list) #quitamos al elemento de menor n
-        #current = open_list.popleft() 
+        current = heapq.heappop(open_list)[1]##
         in_closed_list[current] = True
-        neighbors = [current + req.map.info.width, current - req.map.info.width, current + 1, current - 1]
-        dist = distances[current] + 1
+        neighbors = [current + map.info.width, current - map.info.width, current + 1, current - 1]
         for n in neighbors:
-            if req.map.data[n] > 40 or req.map.data[n] < 0 or in_closed_list[n]:
+            if map.data[n] > 40 or map.data[n] < 0 or in_closed_list[n]:
                 continue
+            dist = distances[current] + 1 + map.data[n]##
             if dist < distances[n]:
                 distances[n]    = dist
                 parent_nodes[n] = current
             if not in_open_list[n]:
                 in_open_list[n] = True
-                heapq.heappush(open_list,n)
-                #open_list.append(n)
+                heapq.heappush(open_list, (dist, n))###
             steps += 1
 
     if current != goal_idx:
-        print ("Cannot calculate path :'(")
+        print "Cannot calculate path :'("
         return None
-
-
-
-    
     ####
     print "Path calculated after " + str(steps) + " steps."
     msg_path = Path()
@@ -121,29 +113,16 @@ def callback_dijkstra(req):
     # Store the resulting path in the 'msg_path' variable
     # Return the appropiate response
     # 
-    
+    while parent_nodes[current] != -1:
+        p = PoseStamped()
+        p.pose.position.x = (current%map.info.width)*map.info.resolution + map.info.origin.position.x
+        p.pose.position.y = (current/map.info.width)*map.info.resolution + map.info.origin.position.y
+        msg_path.poses.insert(0,p)
+        current = parent_nodes[current]
     ####
     pub_path = rospy.Publisher('/navigation/path_planning/calculated_path', Path, queue_size=10)
     pub_path.publish(msg_path)
     return CalculatePathResponse(msg_path)
-
-
-
-
-'''
-ALGORITMO A*
-
-Se quiere calcular la mejor ruta, esta es la de menor costo, es decir
-la de la distancia más corta entre el inicio y el punto meta.
-
-Cada nodo tiene una distancia con respecto a otro, este es el costo.
-
-HEURISTICA --> Distancia que hay entre uno de los nodos al destino.
-
-g --> es el costo del nodo anterior 
-h --> heuristica
-'''
-
 
 def callback_a_star(req):
     print "Calculating path A-Star" ####
@@ -160,51 +139,45 @@ def callback_a_star(req):
     # Use the 'steps' variable to store the total steps needed for calculations
     # HINT: Use a heap structure to keep track of the node with the smallest f-value
     #
+    start_idx  = int((req.start.pose.position.x - map.info.origin.position.x)/map.info.resolution)
+    start_idx += int((req.start.pose.position.y - map.info.origin.position.y)/map.info.resolution)*map.info.width
+    goal_idx   = int((req.goal.pose.position.x  - map.info.origin.position.x)/map.info.resolution)
+    goal_idx  += int((req.goal.pose.position.y  - map.info.origin.position.y)/map.info.resolution)*map.info.width
     
-    
-    start_idx  = int((req.start.pose.position.x - req.map.info.origin.position.x)/req.map.info.resolution)
-    start_idx += int((req.start.pose.position.y - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
-    goal_idx   = int((req.goal.pose.position.x  - req.map.info.origin.position.x)/req.map.info.resolution)
-    goal_idx  += int((req.goal.pose.position.y  - req.map.info.origin.position.y)/req.map.info.resolution)*req.map.info.width
-
-    open_list      = []
-    #open_list = deque()
-    in_open_list   = [False]*len(req.map.data)
-    in_closed_list = [False]*len(req.map.data)
-    distances      = [sys.maxint]*len(req.map.data)
-    parent_nodes   = [-1]*len(req.map.data)
+    open_list      = []#####
+    in_open_list   = [False]*len(map.data)
+    in_closed_list = [False]*len(map.data)
+    distances      = [sys.maxint]*len(map.data)
+    parent_nodes   = [-1]*len(map.data)
     
     current = start_idx
-    heapq.heappush(open_list,start_idx) #aqui agragamos el nodo inicial a la lista abierta
-    #open_list.append(start_idx)
+    heapq.heappush(open_list, (0, start_idx))##
     in_open_list[start_idx] = True
     distances[start_idx]    = 0
-    
+
     while len(open_list) > 0 and current != goal_idx:
-        current = heapq.heappop(open_list) #quitamos al elemento de menor f
-        #current = open_list.popleft() 
+        current = heapq.heappop(open_list)[1]##
         in_closed_list[current] = True
-        neighbors = [current + req.map.info.width, current - req.map.info.width, current + 1, current - 1]
-        dist = distances[current] + 1
+        neighbors = [current + map.info.width, current - map.info.width, current + 1, current - 1]
         for n in neighbors:
-            if req.map.data[n] > 40 or req.map.data[n] < 0 or in_closed_list[n]:
+            if map.data[n] > 40 or map.data[n] < 0 or in_closed_list[n]:
                 continue
+            dist = distances[current] + 1 + int(map.data[n]/5.0)##
+            f_value = sys.maxint
             if dist < distances[n]:
+                h  = abs(n%map.info.width - goal_idx%map.info.width);
+		h += abs(n/map.info.width - goal_idx/map.info.width);
                 distances[n]    = dist
                 parent_nodes[n] = current
+                f_value = dist + h
             if not in_open_list[n]:
                 in_open_list[n] = True
-                heuristic = abs(start_idx - goal_idx)
-                heapq.heappush(open_list,n + heuristic)
-                #open_list.append(n)
+                heapq.heappush(open_list, (f_value, n))###
             steps += 1
 
     if current != goal_idx:
-        print ("Cannot calculate path :'(")
+        print "Cannot calculate path :'("
         return None
-
-
-
     ####
     print "Path calculated after " + str(steps) + " steps."
     msg_path = Path()
@@ -214,7 +187,12 @@ def callback_a_star(req):
     # Store the resulting path in the 'msg_path' variable
     # Return the appropiate response
     #
-    
+    while parent_nodes[current] != -1:
+        p = PoseStamped()
+        p.pose.position.x = (current%map.info.width)*map.info.resolution + map.info.origin.position.x
+        p.pose.position.y = (current/map.info.width)*map.info.resolution + map.info.origin.position.y
+        msg_path.poses.insert(0,p)
+        current = parent_nodes[current]
     ####
     pub_path = rospy.Publisher('/navigation/path_planning/calculated_path', Path, queue_size=10)
     pub_path.publish(msg_path)
@@ -237,26 +215,3 @@ if __name__ == '__main__':
         pass
     
 
-
-'''
-El algiritmo de la cola heap también se conoce como el algoritmo de 
-la cola con prioridad.
-
-heaps son arboles binarios en el cual cada nodo padre tiene un valor 
-menor o igual que cualquiera de sus hijos.
-
-Una propiedad interesante de una cola heap es que su elemento mas 
-pequeño es siempre la raiz, heap[0].
-
-El método POP regresa el elemento más pequeño, se le llama "min heap"
-
-heap.sort() --> mantiene a la cola heap invariante.
-
-Para crear una cola heap debemos usar una lista inicializada en []
-o podemos transformar una lista en una cola heap con la función 
-heapify()
-
-heapq.heappush(list,item) --> con este método agregamos elementos a la
-a la cola heap, esta cola siempre pondrá el elemento más pequeño al 
-principio, esto lo hace por default.
-'''
