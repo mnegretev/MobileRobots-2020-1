e52263d2e9d50d0998427be3d3b4543efbedbd4b
Simon <sera.-@hotmail.com>
2019-09-23 21:58:18 -0500

    Agrego cambios practica 3

--- catkin_ws/src/students/ramirez_ancona/scripts/practice03.py
+++ catkin_ws/src/students/ramirez_ancona/scripts/practice03.py
@@ -16,7 +16,7 @@ from navig_msgs.srv import SmoothPath
 from navig_msgs.srv import SmoothPathResponse
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "ramirez_ancona"
 
 def callback_smooth_path(req):
     if rospy.has_param("/navigation/path_planning/smoothing_alpha"):
@@ -33,7 +33,6 @@ def callback_smooth_path(req):
     smooth_path.header.frame_id = "map"
     smooth_path.header.stamp    = rospy.Time.now()
     
-    x = old
     #
     # TODO:
     # Write the algorithm to smooth the path by gradient descend algorith
@@ -42,18 +41,50 @@ def callback_smooth_path(req):
     # Store the resulting path in the 'smooth_path' variable.
     # 
 
-    tol = 0.00001
-    gradient_mag = tol + 1
-    delta = 
-    
+    smooth_path = req.path
 
-    while gradient_mag > tol:
-	grad_0 = aplpha*(smooth_path.poses[0].pose.position.x - smooth_path.poses[1].pose.position.x)
+    tol = 0.00001 * len(req.path.poses)
+    gradient_mag = tol + 1
+    delta = 0.5
+    attempts = 1000
+
+    while gradient_mag >= tol and attempts > 0:
+
+	"""grad_0 = aplpha*(smooth_path.poses[0].pose.position.x - smooth_path.poses[1].pose.position.x)
 	grad_0 += beta*(smooth_path.poses[0].pose.position.x - old_path.poses[0].pose.position.x)
-	new_x_0 -= 
+	new_x_0 -= delta*grad_0
+
+	grad_0 = aplpha*(smooth_path.poses[0].pose.position.y - smooth_path.poses[1].pose.position.y)
+	grad_0 += beta*(smooth_path.poses[0].pose.position.y - old_path.poses[0].pose.position.y)
+	new_y_0 -= delta*grad_0"""
+	gradient_mag = 0
+	
+	for i in range(1,len(req.path.poses)-1):
+
+		x_old = req.path.poses[i].pose.position.x
+		y_old = req.path.poses[i].pose.position.y
+
+		x_new_prev = smooth_path.poses[i-1].pose.position.x
+		y_new_prev = smooth_path.poses[i-1].pose.position.y
+
+		x_new_i = smooth_path.poses[i].pose.position.x
+		y_new_i = smooth_path.poses[i].pose.position.y
+
+		x_new_next = smooth_path.poses[i+1].pose.position.x
+		y_new_next = smooth_path.poses[i+1].pose.position.y
+
+		grad_x = beta*(x_new_i - x_old) + alpha*(2*x_new_i - x_new_prev - x_new_next)
+		grad_y = beta*(y_new_i - y_old) + alpha*(2*y_new_i - y_new_prev - y_new_next)
+
+		x_new_i -= delta*grad_x
+		y_new_i -= delta*grad_y
+
+		gradient_mag += abs(grad_x) + abs(grad_y)
+	#print "Gradient_mag " + str(gradient_mag)
+	attempts -= 1	    
 
     ####
-    #print "Path smoothed after " + str(attempts) + " steps"
+    print "Path smoothed after " + str(attempts) + " steps"
     pub_path = rospy.Publisher('/navigation/path_planning/smooth_path', Path, queue_size=10)
     pub_path.publish(smooth_path)
     return SmoothPathResponse(smooth_path)
@@ -72,3 +103,14 @@ if __name__ == '__main__':
     except rospy.ROSInterruptException:
         pass
     
+
+
+
+
+
+
+
+
+
+
+
