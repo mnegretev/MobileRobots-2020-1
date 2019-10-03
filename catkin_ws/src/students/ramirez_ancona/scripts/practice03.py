28cddc6dadd9ada96cf3b1a2a1fecfe4f11d8c13
Simon <sera.-@hotmail.com>
2019-10-03 10:37:43 -0500

    Agrego práctica 3 modificada

--- catkin_ws/src/students/ramirez_ancona/scripts/practice03.py
+++ catkin_ws/src/students/ramirez_ancona/scripts/practice03.py
@@ -4,13 +4,13 @@
 # PRACTICE 3 - PATH SMOOTHING BY GRADIENT DESCEND
 #
 # Instructions:
-# Write the code necessary to smooth a path given by a sequence of points
+# Complete the code necessary to smooth a path given by a sequence of points
 # using a gradient descend algorithm.
 #
 
 import sys
 import rospy
-import math
+import copy
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 from navig_msgs.srv import SmoothPath
@@ -18,6 +18,51 @@ from navig_msgs.srv import SmoothPathResponse
 from collections import deque
 
 NAME = "ramirez_ancona"
+
+def get_smooth_path(original_path, alpha, beta):
+    #
+    # TODO:
+    # Complete the algorithm to smooth the 'original_path' and return a new path.
+    # Path is composed of a set of points [x,y]. For example, the following line
+    # [xo_i,yo_i] = original_path[i]
+    # stores the x,y coordinates of the i-th point of the original path
+    # in the variables xo_i and yo_i respectively. 
+    #
+    #
+    smooth_path  = copy.deepcopy(original_path)            # At the beginnig, the smooth path is the same than the original path.
+    tolerance    = 0.00001                                 # If gradient magnitude is less than a tolerance, we consider.
+    gradient_mag = tolerance + 1                           # we have reached the local minimum.
+    gradient     = [[0,0] for i in range(len(smooth_path))]# Gradient has N components of the form [x,y]. 
+    epsilon      = 0.5                                     # This variable will weight the calculated gradient.
+
+    while gradient_mag > tolerance:
+        #First and last components are not calculated since start and goal points remain the same. 
+        for i in range(1, len(original_path)-1):
+            #
+            # TODO:
+            # Calculate the i-th component of gradient. You need the following variables:
+            [xo_i, yo_i]   = original_path[i] # i-th point of the original path
+            [xn_i, yn_i]   = smooth_path  [i] # i-th point of the smooth path
+            [xn_in, yn_in] = smooth_path[i+1] # (i+1)-th point of the smooth path
+            [xn_ip, yn_ip] = smooth_path[i-1] # (i-1)-th point of the smooth path  
+            #Calculate the X-value of the i-th component of the gradient (see lecture notes)
+            #Calcualte the Y-value of the i-th component of the gradient (see lecture notes)
+	    grad_x = alpha*(2*xn_i-xn_ip-xn_in)
+	    grad_x +=beta*(xn_i-xo_i)
+	    grad_y = alpha*(2*yn_i-yn_ip-yn_in)
+	    grad_y +=beta*(yn_i-yo_i)
+            gradient[i] = [grad_x, grad_y]
+            
+        # We change the value of the i-th point of the smooth path in the opposite direction of the gradient:
+        for i in range(len(smooth_path)):
+            smooth_path[i][0] -= epsilon*gradient[i][0]
+            smooth_path[i][1] -= epsilon*gradient[i][1]
+
+        # Finally, we calculate the magnitude of the gradient to check if we have already reached the local minimum: 
+        gradient_mag = 0
+        for i in range(len(gradient)):
+            gradient_mag += abs(gradient[i][0]) + abs(gradient[i][1]) #We are using the norm-1
+    return smooth_path
 
 def callback_smooth_path(req):
     if rospy.has_param("/navigation/path_planning/smoothing_alpha"):
@@ -28,70 +73,28 @@ def callback_smooth_path(req):
         beta = rospy.get_param("/navigation/path_planning/smoothing_beta")
     else:
         beta = 0.5
+
+    path = []
+    for i in range(len(req.path.poses)):
+        x = req.path.poses[i].pose.position.x
+        y = req.path.poses[i].pose.position.y
+        path.append([x,y])
+
     print "Smoothing a " + str(len(req.path.poses)) + " points path with alpha=" + str(alpha) + " and beta=" + str(beta)
-
-    smooth_path = Path()
-    smooth_path.header.frame_id = "map"
-    smooth_path.header.stamp    = rospy.Time.now()
-    
-    #
-    # TODO:
-    # Write the algorithm to smooth the path by gradient descend algorith
-    # using as cost function the sum of the distance between point in the new path
-    # plus the distance bewteen points in the new and old paths.
-    # Store the resulting path in the 'smooth_path' variable.
-    # 
-    #for i in range(len(req.path.poses)):
-     #   smooth_path.poses.append(req.path.poses[i])
-    smooth_path.poses = req.path.poses
-
-    tol = 0.001 * len(req.path.poses) 
-    gradient_mag = 1
-    delta = 0.5
-    attempts = 10000
-
-    #print len(req.path.poses)
-    #print len(smooth_path.poses)
-
-    while gradient_mag > tol and attempts > 0:
-
-	gradient_mag = 0
-	
-	for i in range(1,len(req.path.poses)-2):
-
-		x_old = req.path.poses[i].pose.position.x
-		y_old = req.path.poses[i].pose.position.y
-
-		x_new_prev = smooth_path.poses[i-1].pose.position.x
-		y_new_prev = smooth_path.poses[i-1].pose.position.y
-
-		x_new_i = smooth_path.poses[i].pose.position.x
-		y_new_i = smooth_path.poses[i].pose.position.y
-
-		x_new_next = smooth_path.poses[i+1].pose.position.x
-		y_new_next = smooth_path.poses[i+1].pose.position.y
-
-		grad_x =  beta*(x_new_i - x_old)
-		grad_x += alpha*(2*x_new_i - x_new_prev - x_new_next)
-		grad_y =  beta*(y_new_i - y_old) 
-		grad_y += alpha*(2*y_new_i - y_new_prev - y_new_next)
-
-		x_new_i -= delta*grad_x
-		y_new_i -= delta*grad_y
-		#print grad_y
-		#print "Gradiente dentro for" + str(i) + " = " + str(gradient_mag)
-		gradient_mag += math.sqrt(grad_x**2 + grad_y**2)
-		#if gradient_mag < tol: print str(i)
-	
-        #print "Gradient_mag " + str(gradient_mag)
-        #print tol
-	attempts -= 1	    
-
-    ####
-    print "Path smoothed after " + str(attempts) + " steps"
+    smooth_path = get_smooth_path(path, alpha, beta)
+    print "Path smoothed"
+    msg_smooth_path = Path()
+    msg_smooth_path.header.frame_id = "map"
+    for i in range(len(path)):
+        p = PoseStamped()
+        p.pose.position.x = smooth_path[i][0]
+        p.pose.position.y = smooth_path[i][1]
+        msg_smooth_path.poses.append(p)
+        
+    msg_smooth_path.header.stamp    = rospy.Time.now()
     pub_path = rospy.Publisher('/navigation/path_planning/smooth_path', Path, queue_size=10)
-    pub_path.publish(smooth_path)
-    return SmoothPathResponse(smooth_path)
+    pub_path.publish(msg_smooth_path)
+    return SmoothPathResponse(msg_smooth_path)
 
 def main():
     print "PRACTICE 03 - " + NAME
@@ -106,15 +109,3 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
-    
-
-
-
-
-
-
-
-
-
-
-
